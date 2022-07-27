7324148fe31596c8d509e36884f4174b47278e1e
Vivaldi Darren Christophlus <vivdc2001@gmail.com>
2022-07-27 09:50:52 +0700

    Initial Commit

---
+++ EasyLearn/EasyLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyLearnApp.swift
+//  EasyLearn
+//
+//  Created by Vivaldi Darren Christophilus on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct EasyLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
